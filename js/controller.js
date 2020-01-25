b3cfa5694159106ca8f1128cc2a65dae91953a0f
Your Name <hurtadojose89@gmail.com>
2020-01-25 18:36:59 +0100

    [#8] - X collisions working

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,12 @@
 class Controller {
 
-    constructor(boardWidth, boardHeight, boardPortion ,level) {
+    constructor(boardWidth, boardHeight, boardPortion, level) {
         this.board = new Board(boardWidth, boardHeight, boardPortion, level, 1.5);
         this.view = new View();
-        this.player =  new Player(this.board.portion,this.board.portion*2,{x:0, y: this.board.height - this.board.portion *2 - this.board.portion },3, 100, 2);
+        this.player = new Player(this.board.portion, this.board.portion * 2, {
+            x: 100,
+            y: this.board.height - this.board.portion * 2 - this.board.portion
+        }, 3, 100, 2);
         this.view.startGame(this.player, this.board);
         this.startListeners();
 
@@ -32,15 +35,15 @@ class Controller {
     movePlayer() {
         clearInterval(this.player.moveInterval);
         this.player.moveInterval = setInterval(() => {
-            switch(this.player.facing) {
+            switch (this.player.facing) {
                 case 'right':
-                    if (this.canPlayerMoveRight()) {
+                    if (this.canPlayerMoveRight([this.player.position.x + this.player.velX, this.player.position.y])) {
                         this.player.moveRight();
                         this.view.moveCameraToRight(this.board, this.player.position.x);
                     }
                     break;
                 case 'left':
-                    if (this.canPlayerMoveLeft()) {
+                    if (this.canPlayerMoveLeft([this.player.position.x - this.player.velX, this.player.position.y])) {
                         this.player.moveLeft();
                         this.view.moveCameraToLeft(this.board, this.player.position.x);
                     }
@@ -49,18 +52,31 @@ class Controller {
         }, 1);
     }
 
-    canPlayerMoveRight() {
-        return true;
+    canPlayerMoveRight(position) {
+        let response = true;
+        this.board.getPosiblesCollitionsInX(position[0]).forEach(brick => {
+            if (brick[0] === this._getRoundedPosition(position[0] + this.player.width / 2 - this.player.velX) && (brick[1] === this._getRoundedPosition(position[1] + this.board.portion) || brick[1] === this._getRoundedPosition(position[1]))) {
+                response = false;
+            }
+        });
+
+        return response;
     }
 
-    canPlayerMoveLeft() {
+    canPlayerMoveLeft(position) {
+        let response = true;
+        this.board.getPosiblesCollitionsInX(position[0]).forEach(brick => {
+            if (this._getRoundedPosition(position[0] + this.player.velX * 10) === brick[0] + this.board.portion && (brick[1] === this._getRoundedPosition(position[1] + this.board.portion) || brick[1] === this._getRoundedPosition(position[1]))) {
+                response = false;
+            }
+        });
 
-        return true;
+        return response;
     }
 
     stopMovingPlayerListener() {
         this.view.stopMovingPlayer(() => {
-           this.player.stopMoving();
+            this.player.stopMoving();
         });
     }
 
@@ -75,7 +91,38 @@ class Controller {
         }
     }
 
-    startGameListener() {}
-    resetGameListener() {}
+    startGameListener() {
+    }
+
+    resetGameListener() {
+    }
+
+    /*
+      Redondea el número al mas cercano a la porcion
+      302 -> 300
+      326 -> 350
+      356 -> 350
+      389 -> 400
+   */
+    _getRoundedPosition(position) {
+
+        const portion = this.board.portion;
+        if (position % 100 < portion) {
+            if (position % 100 < portion / 2) {
+                position = position - (position % 100);
+            } else {
+                position = (position - (position % 100)) + portion;
+            }
+
+        } else {
+            if (position % 100 < portion * 2 - ((portion * 2) / 4)) {
+                position = (position - (position % 100)) + portion;
+            } else {
+                position = (position - (position % 100)) + portion * 2;
+            }
+        }
+
+        return position;
+    }
 
 }
